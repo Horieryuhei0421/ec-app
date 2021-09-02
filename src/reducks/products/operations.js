6a4e9befc2ffeede9920902dc0e3d11721576d79
Horieryuhei0421 <horiejr421@icloud.com>
2021-09-02 12:40:20 +0900

    order関数完成

--- src/reducks/products/operations.js
+++ src/reducks/products/operations.js
@@ -32,12 +32,13 @@ export const fetchProducts = () => {
 }
 
 //引数はカート内の商品,合計金額
-export const orderProduct = (productsInCart) => {
+export const orderProduct = (productsInCart, price) => {
   return async (dispatch, getState) => {
+
     const uid = getState().users.uid;
     const userRef = db.collection('users').doc(uid);
     const timestamp = FirebaseTimestamp.now();
-    let products = {};
+    let products = [];
     let soldOutProducts = [];
 
     const batch = db.batch();
@@ -45,9 +46,9 @@ export const orderProduct = (productsInCart) => {
     for (const product of productsInCart) {
       const snapshot = await productsRef.doc(product.productId).get();
       const sizes = snapshot.data().sizes;
-      // ↑データベース内の指定した商品のサイズ一覧
+
+      // Create a new array of the product sizes
       const updateSizes = sizes.map(size => {
-      //  ↓データベースのサイズを回してそのサイズ名とカート内のサイズ名が一致したら-1
         if (size.size === product.size) {
           if (size.quantity === 0) {
             soldOutProducts.push(product.name);
@@ -61,43 +62,54 @@ export const orderProduct = (productsInCart) => {
           return size
         }
       });
-      products[product.productId] = {
+
+      products.push({
         id: product.productId,
         images: product.images,
         name: product.name,
         price: product.price,
         size: product.size
-      };
-      batch.update(productsRef.doc(product.productId), {sizes: updateSizes});
+      });
+
+      batch.update(productsRef.doc(product.productId), { sizes: updateSizes });
       batch.delete(userRef.collection('cart').doc(product.cartId));
-  }
-   if (soldOutProducts.length > 0) {
+    }
+
+    if (soldOutProducts.length > 0) {
       const errorMessage = (soldOutProducts.length > 1) ? soldOutProducts.join('と') : soldOutProducts[0];
       alert('大変申し訳ありません。' + errorMessage + 'が在庫切れとなったため注文処理を中断しました。');
       return false
     } else {
-      // 注文履歴データを作成
-      const orderRef = userRef.collection('orders').doc();
-      const date = timestamp.toDate();
-      // 配送日を3日後に設定
-      const shippingDate = FirebaseTimestamp.fromDate(new Date(date.setDate(date.getDate() + 3)));
 
-      const history = {
-        amount: price,
-        created_at: timestamp,
-        id: orderRef.id,
-        products: products,
-        shipping_date: shippingDate,
-        updated_at: timestamp
-      };
+      batch.commit()
+        .then(() => {
+          // 注文履歴データを作成
+          const orderRef = userRef.collection('orders').doc();
+          const date = timestamp.toDate();
+          // 配送日を3日後に設定
+          const shippingDate = FirebaseTimestamp.fromDate(new Date(date.setDate(date.getDate() + 3)));
 
-      orderRef.set(history);
-       dispatch(push("/order/complete");
-    }).catch(() => {
-            dispatch(hideLoadingAction());
-            alert('注文処理に失敗しました。通信環境をご確認のうえ、もう一度お試しください。')
-    })
-}}
+          const history = {
+            amount: price,
+            created_at: timestamp,
+            id: orderRef.id,
+            products: products,
+            shipping_date: shippingDate,
+            updated_at: timestamp
+          };
+
+          orderRef.set(history)
+          dispatch(push("order/complete"))
+
+        }).catch(() => {
+          alert('注文処理に失敗しました。通信環境をご確認のうえ、もう一度お試しください。')
+          return false
+        })
+
+    }
+  }
+}
+
 
 export const saveProduct = (id, name, description, category, gender, price, images, sizes) => {
   return async (dispatch) => {
